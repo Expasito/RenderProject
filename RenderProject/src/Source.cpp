f798dcf5208d964362b1b7cbbe96b503f0af4317
Expasito <93100379+Expasito@users.noreply.github.com>
2023-07-21 23:19:50 -0700

    Testing a binary heap to get a better datastructure

--- RenderProject/src/Source.cpp
+++ RenderProject/src/Source.cpp
@@ -66,8 +66,165 @@ private:
 };
 
 
+class MinHeap {
+public:
+	struct Element {
+		int key;
+		int value;
+	};
+
+	Element* arr;
+	int elements;
+	int size;
+
+	MinHeap(int size_) {
+		arr = (Element*)malloc(sizeof(Element) * size_);
+		elements = 1;
+		size = size_;
+
+		for (int i = 0; i < size; i++) {
+			arr[i] = { 0,0 };
+		}
+
+	}
+
+	int parent(int key) {
+		return key / 2;
+	}
+
+	int left(int key) {
+		return key * 2;
+	}
+
+	int right(int key) {
+		return key * 2 + 1;
+	}
+
+	int compare(int key, int key2) {
+		return key < key2;
+	}
+
+	void add(int key, int value) {
+		arr[elements] = { key,value };
+		bubbleup(elements);
+		elements++;
+		std::cout << "see here\n";
+		see();
+		std::cout << "end see\n";
+	}
+
+	Element remove() {
+		Element out = arr[1];
+		//std::cout << "key: " << arr[elements - 1].key << "\n";
+		arr[1] = arr[elements-1];
+		//std::cout << elements << "\n";
+		//std::cout << "herererererer\n";
+		//see();
+		//std::cout << "sfjsfjskfjs;lfsf\n";
+		bubbledown(1);
+		elements--;
+		return out;
+	}
+
+	void bubbledown(int index) {
+		while (index < elements) {
+			int leftkey = arr[left(index)].key;
+			int rightkey = arr[right(index)].key;
+			int larger = leftkey < rightkey ? rightkey : leftkey;
+			int dir = leftkey < rightkey ? right(index) : left(index);
+			std::cout << "   index: " << index << " new: " << dir << "\n";
+			if (larger > arr[index].key) {
+				// flip the two and repeat
+				Element temp = arr[dir];
+				arr[dir] = arr[index];
+				arr[index] = temp;
+				index = dir;
+			}
+			else {
+				break;
+			}
+			//std::cout << "seeing now\n";
+			//see();
+			//std::cout << "stop seeing\n";
+
+		}
+
+	}
+
+	void bubbleup(int index) {
+		std::cout << "         " << parent(index) << " " << index << "\n";
+		std::cout << "         " << arr[parent(index)].key << " " << arr[index].key << "\n";
+		while (arr[parent(index)].key > arr[index].key && index > 1) {
+			std::cout << "     flipping\n";
+			// flip the two elements
+			Element temp = arr[parent(index)];
+			arr[parent(index)] = arr[index];
+			arr[index] = temp;
+			index = parent(index);
+		}
+	}
+
+	void see() {
+		for (int i = 1; i < elements; i++) {
+			std::cout << arr[i].key << "\n";
+		}
+	}
+
+	
+
+
+
+private:
+
+
+};
+
 //#include <Render/Instances.h>
 int main() {
+
+	MinHeap mh(8);
+
+	mh.see();
+
+	mh.add(5, 1);
+	mh.add(2, 1);
+
+	mh.add(1, 1);
+	mh.add(3, 1);
+	mh.add(8, 1);
+
+	mh.add(4, 1);
+	mh.add(9, 1);
+
+	//mh.add(11, 1);
+	//mh.add(0, 1);
+
+
+	std::cout << "final\n";
+
+	mh.see();
+
+	//std::cout << "removing now\n";
+
+	//mh.remove();
+	//while (mh.elements != 0) {
+	//	MinHeap::Element out = mh.remove();
+	//	std::cout << "key: " << out.key << "\n";
+
+
+	//}
+
+	//mh.see();
+
+
+
+	exit(1);
+
+
+	// LOOK INTO USING A BINARY HEAP
+	// all elements are there in the array sequentially
+	// get, set, remove and add are logn
+	// https://en.wikipedia.org/wiki/Binary_heap#:~:text=The%20number%20of%20operations%20required,complexity%20of%20O(1).
 
 	//Instances insts(5);
 	//int index__ = 0;
